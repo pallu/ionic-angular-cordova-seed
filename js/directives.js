c00ab60adb9c192f6491eb5fc19427fab99a1b9c
pallu <pallu@geocities.com>
2014-03-25 10:01:57 -0400

    Commit

---
+++ js/directives.js
@@ -0,0 +1,6 @@
+﻿angular.module('starter.directives', [])
+.directive('ncgRequestVerificationToken', ['$http', function ($http) {
+    return function (scope, element, attrs) {
+        $http.defaults.headers.common['RequestVerificationToken'] = attrs.ncgRequestVerificationToken || "no request verification token";
+    };
+}]);
